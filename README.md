d482bebcb7b51a71087d0a148b78bdf2975972e2
Geoff Nelson <geoffreysnelson@gmail.com>
2023-07-25 09:01:26 -0700

    final update

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 [![Flutter](https://img.shields.io/badge/Made%20with-Flutter-blue.svg)](https://flutter.dev/)
 [![MIT License](https://badges.frapsoft.com/os/mit/mit.svg?v=103)](https://opensource.org/licenses/mit-license.php)
 
-![](https://www.outdoorcoders.com/images/flutterbits_readme.png)
 
-Welcome to the Flutter Image Widget repository! This repository provides a comprehensive collection of examples, best practices, and resources for effectively using the Image widget in Flutter to create stunning and interactive mobile app UIs.
+<img src='https://www.outdoorcoders.com/images/flutterbits_readme.png' width=450>
+
+Welcome to the Flutter Bits Image Widget repository! This repository provides a comprehensive collection of examples, best practices, and resources for effectively using the Image widget in Flutter to create stunning and interactive mobile app UIs.
 
--- lib/fb_images.dart
+++ lib/fb_images.dart
@@ -5,18 +5,57 @@ class FBImages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String baseURL = 'https://images.unsplash.com/photo-1502175353174-a7a70e73b362?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2026&q=80';
+    String networkAssetURL = 'https://images.unsplash.com/photo-1502175353174-a7a70e73b362?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2026&q=80';
     String assetImage = 'assets/images/taylor.jpg';
+    String avatarAssetImage = 'assets/images/funko.jpeg';
+
     return SingleChildScrollView(
       child: Container(
         color: Colors.black,
         child: Column(
           children: [
+            const Center(
+                child: Text(
+              'Network Image',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            )),
             Padding(
               padding: const EdgeInsets.all(20.0),
               child: Center(
                 child: Image.network(
-                  baseURL,
+                  networkAssetURL,
+                  errorBuilder: (context, error, stackTrace) => const Text(
+                    'ERROR',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+              ),
+            ),
+            const Center(
+                child: Text(
+              'Asset Image',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            )),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Center(
+                child: Image.asset(
+                  assetImage,
+                ),
+              ),
+            ),
+            const Center(
+              child: Text(
+                'Asset Image Sized',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
                 ),
               ),
             ),
@@ -25,18 +64,98 @@ class FBImages extends StatelessWidget {
               child: Center(
                 child: Image.asset(
                   assetImage,
-                  height: 300,
+                  height: 400,
                   width: 300,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const Center(
+              child: Text(
+                'Asset Image Fill vs Cover',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
                 ),
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(20.0),
-              child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      const Text(
+                        'Fill',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        child: Image.asset(
+                          assetImage,
+                          height: 200,
+                          width: 100,
+                          fit: BoxFit.fill,
+                          //fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 25,
+                  ),
+                  Column(
+                    children: [
+                      const Text(
+                        'Cover',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        child: Image.asset(
+                          assetImage,
+                          fit: BoxFit.cover,
+                          height: 200,
+                          width: 100,
+                          //fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const Center(
+              child: Text(
+                'Rounded Corner Image',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(40.0)),
                 child: Image.asset(
                   assetImage,
-                  fit: BoxFit.cover,
                   //fit: BoxFit.cover,
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            const Center(
+              child: Text(
+                'Circular Image CircleAvatar',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
                 ),
               ),
             ),
@@ -54,12 +173,21 @@ class FBImages extends StatelessWidget {
                 ),
               ),
             ),
+            const Center(
+              child: Text(
+                'Circular Image Sized',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.all(20.0),
               child: Center(
                 child: ClipOval(
                   child: Image.asset(
-                    'assets/images/gn_funko.jpeg',
+                    avatarAssetImage,
                     height: 100,
                     width: 100,
                     fit: BoxFit.cover,
@@ -67,16 +195,8 @@ class FBImages extends StatelessWidget {
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(20.0)),
-                child: Image.asset(
-                  assetImage,
-                  //fit: BoxFit.cover,
-                  fit: BoxFit.fill,
-                ),
-              ),
+            const SizedBox(
+              height: 300,
             )
           ],
         ),
